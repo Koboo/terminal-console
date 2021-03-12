ef0de9071643757eea2a71c88c4be9d6f61f569f
Koboo <jakob.koerber@gmail.com>
2021-03-12 12:17:11 +0100

    Add `stretch`-methods

--- src/main/java/eu/koboo/terminal/TerminalConsole.java
+++ src/main/java/eu/koboo/terminal/TerminalConsole.java
@@ -1,7 +1,6 @@
 package eu.koboo.terminal;
 
 import eu.koboo.terminal.command.Command;
-import eu.koboo.terminal.command.ICommand;
 import eu.koboo.terminal.util.ConsoleColor;
 import eu.koboo.terminal.util.ConsoleFormatter;
 import eu.koboo.terminal.util.ConsoleLevel;
@@ -22,8 +21,11 @@ import java.util.logging.*;
 
 public class TerminalConsole {
 
+    private static TerminalConsole INSTANCE;
+
     private final Logger logger = Logger.getLogger(TerminalConsole.class.getName());
-    private final String applicationName;
+
+    private final ConsoleBuilder consoleBuilder;
     private final ExecutorService commandExecutor;
     private final AtomicReference<Boolean> active;
     private final HashMap<String, Command> commandRegistry;
@@ -33,15 +35,17 @@ public class TerminalConsole {
     private LineReader lineReader;
     private String contentGrep;
 
-    public TerminalConsole(String appName, boolean externalLog) {
-        this.applicationName = appName;
-        this.commandExecutor = Executors.newFixedThreadPool(4);
-        this.consoleLevel = ConsoleLevel.INFO;
+    protected TerminalConsole(ConsoleBuilder consoleBuilder) {
+        INSTANCE = this;
+
+        this.consoleBuilder = consoleBuilder;
+        this.commandExecutor = Executors.newFixedThreadPool(consoleBuilder.getCliThreads());
+        this.consoleLevel = consoleBuilder.getInitialLevel();
         this.active = new AtomicReference<>(true);
         this.commandRegistry = new HashMap<>();
         this.contentGrep = null;
 
-        File logDir = new File("logs/");
+        File logDir = new File(consoleBuilder.getLogDirectory());
         if (!logDir.exists())
             logDir.mkdirs();
         int count = 1;
@@ -69,7 +73,7 @@ public class TerminalConsole {
             this.lineReader = LineReaderBuilder
                     .builder()
                     .terminal(this.terminal)
-                    .variable(LineReader.SECONDARY_PROMPT_PATTERN, ConsoleColor.DARK_YELLOW.getAnsiColor() + " > " + ConsoleColor.RESET.getAnsiColor())
+                    .variable(LineReader.SECONDARY_PROMPT_PATTERN, consoleBuilder.getPrompt())
                     .build();
         } catch (Exception e) {
             e.printStackTrace();
@@ -81,9 +85,9 @@ public class TerminalConsole {
         Handler handler = new Handler() {
             @Override
             public void publish(LogRecord record) {
-                if(externalLog) {
+                if (consoleBuilder.isShowExternal()) {
                     if (record.getThrown() != null)
-                        error(record.getMessage() + " [EXT]", record.getThrown());
+                        log(ConsoleLevel.EXT, record.getMessage(), record.getThrown());
                     else
                         log(ConsoleLevel.EXT, record.getMessage());
                 }
@@ -128,7 +132,7 @@ public class TerminalConsole {
             Runtime.getRuntime().addShutdownHook(new Thread(() -> active.set(false)));
             while (active.get() && !Thread.currentThread().isInterrupted()) {
                 if (lineReader != null) {
-                    String line = lineReader.readLine(ConsoleColor.DARK_YELLOW.getAnsiColor() + " > " + ConsoleColor.RESET.getAnsiColor());
+                    String line = lineReader.readLine(consoleBuilder.getPrompt());
                     line = line.trim();
                     if (!executeCLICommand(line)) {
                         info("No command registered: '" + line + "'! Type 'help' for help.");
@@ -145,11 +149,12 @@ public class TerminalConsole {
             String[] args = input.split(" ");
             String inputCommand = args[0].toLowerCase();
             args = Arrays.copyOfRange(args, 1, args.length);
-            ICommand iCommand = commandRegistry.getOrDefault(inputCommand, null);
-            if (iCommand != null) {
+            Command command = commandRegistry.getOrDefault(inputCommand, null);
+            if (command != null) {
                 String[] finalArgs = args;
-                cmd("cli-execute <> '" + input + "'");
-                commandExecutor.execute(() -> iCommand.execute(inputCommand, finalArgs));
+                if (consoleBuilder.isShowExecute())
+                    all("cli-execute <> '" + input + "'");
+                commandExecutor.execute(() -> command.execute(inputCommand, finalArgs));
                 return true;
             }
         } catch (Exception e) {
@@ -159,44 +164,13 @@ public class TerminalConsole {
     }
 
     private boolean shouldPrint(ConsoleLevel consoleLevel, String message) {
-        if (this.consoleLevel == ConsoleLevel.NONE)
+        if (this.consoleLevel == ConsoleLevel.OFF)
             return false;
-        if (consoleLevel == ConsoleLevel.CMD)
+        if (consoleLevel == ConsoleLevel.ALL)
             return true;
         if (contentGrep != null)
             return message.contains(contentGrep);
         return this.consoleLevel.level() <= consoleLevel.level();
-    }
-
-    public void log(ConsoleLevel consoleLevel, String message, Throwable t) {
-        if (shouldPrint(consoleLevel, message)) {
-            if (t != null) {
-                StringWriter sw = new StringWriter();
-                t.printStackTrace(new PrintWriter(sw));
-                String errorString = sw.toString();
-                message = message + ": " + errorString;
-            }
-
-            final long current = System.currentTimeMillis();
-            String logTimeString = new SimpleDateFormat("HH:mm:ss").format(current);
-            String logDateString = new SimpleDateFormat("yyyy-MM-dd").format(current);
-            message = "&8[&7" + logDateString + " &8- &7" + logTimeString + "&8] " +
-                    "&8[&b" + this.applicationName + "&8] " +
-                    "[&6" + consoleLevel.name().toLowerCase() + "&8] &r" +
-                    message + "&r" + System.lineSeparator();
-            logger.info(ConsoleColor.removeColor(message));
-            message = ConsoleColor.parseColor(message);
-            if (terminal == null) {
-                System.out.println(message);
-                return;
-            }
-            if (lineReader == null) {
-                terminal.writer().println(message);
-                terminal.flush();
-                return;
-            }
-            lineReader.printAbove(message);
-        }
     }
 
     public void clearConsole() {
@@ -222,56 +196,142 @@ public class TerminalConsole {
         this.contentGrep = null;
     }
 
+    public void log(ConsoleLevel consoleLevel, String message, Throwable t) {
+        if (shouldPrint(consoleLevel, message)) {
+            if (t != null) {
+                StringWriter sw = new StringWriter();
+                t.printStackTrace(new PrintWriter(sw));
+                String errorString = sw.toString();
+                message = message + ": " + errorString;
+            }
+
+            final long current = System.currentTimeMillis();
+            String logTimeString = new SimpleDateFormat("HH:mm:ss").format(current);
+            String logDateString = new SimpleDateFormat("yyyy-MM-dd").format(current);
+            message = "&8[&7" + logDateString + " &8- &7" + logTimeString + "&8] " +
+                    "&8[&b" + this.consoleBuilder.getAppName() + "&8] " +
+                    "[&6" + consoleLevel.name().toLowerCase() + "&8] &r" +
+                    message + "&r" + System.lineSeparator();
+            logger.info(ConsoleColor.removeColor(message));
+            message = ConsoleColor.parseColor(message);
+            if (terminal == null) {
+                System.out.println(message);
+                return;
+            }
+            if (lineReader == null) {
+                terminal.writer().println(message);
+                terminal.flush();
+                return;
+            }
+            lineReader.printAbove(message);
+        }
+    }
+
     public void log(ConsoleLevel consoleLevel, String message) {
         log(consoleLevel, message, null);
-    }
-
-    public void alert(String message) {
-        log(ConsoleLevel.ALERT, message);
-    }
-
-    public void cmd(String message) {
-        log(ConsoleLevel.CMD, message);
-    }
-
-    public void info(String message) {
-        log(ConsoleLevel.INFO, message);
-    }
-
-    public void debug(String message) {
-        log(ConsoleLevel.DEBUG, message);
-    }
-
-    public void error(String message, Throwable e) {
-        log(ConsoleLevel.ERROR, message, e);
-    }
-
-    public void error(String message) {
-        log(ConsoleLevel.ERROR, message);
-    }
-
-    public void warn(String message) {
-        log(ConsoleLevel.WARN, message);
-    }
-
-    public void warn(String message, Throwable e) {
-        log(ConsoleLevel.WARN, message, e);
     }
 
     public void trace(String message) {
         log(ConsoleLevel.TRACE, message);
     }
 
+    public void debug(String message) {
+        log(ConsoleLevel.DEBUG, message);
+    }
+
+    public void info(String message) {
+        log(ConsoleLevel.INFO, message);
+    }
+
+    public void warn(String message) {
+        log(ConsoleLevel.WARN, message);
+    }
+
+    public void error(String message) {
+        log(ConsoleLevel.ERROR, message);
+    }
+
+    public void fatal(String message) {
+        log(ConsoleLevel.FATAL, message);
+    }
+
+    public void all(String message) {
+        log(ConsoleLevel.ALL, message);
+    }
+
+
     public void trace(String message, Throwable e) {
         log(ConsoleLevel.TRACE, message, e);
     }
 
-    public ConsoleLevel getLogLevel() {
+    public void debug(String message, Throwable e) {
+        log(ConsoleLevel.DEBUG, message, e);
+    }
+
+    public void info(String message, Throwable e) {
+        log(ConsoleLevel.INFO, message, e);
+    }
+
+    public void warn(String message, Throwable e) {
+        log(ConsoleLevel.WARN, message, e);
+    }
+
+    public void error(String message, Throwable e) {
+        log(ConsoleLevel.ERROR, message, e);
+    }
+
+    public void fatal(String message, Throwable e) {
+        log(ConsoleLevel.FATAL, message, e);
+    }
+
+    public void all(String message, Throwable e) {
+        log(ConsoleLevel.ALL, message, e);
+    }
+
+
+    public ConsoleLevel getLevel() {
         return consoleLevel;
     }
 
     public void setLevel(ConsoleLevel consoleLevel) {
         this.consoleLevel = consoleLevel;
+    }
+
+    public String stretchLeft(String message, int length, String stretchEnd) {
+        if (message.length() < length)
+            for (int i = message.length(); i < length; i++) {
+                message = message + " ";
+            }
+
+        else
+            for (int i = message.length(); i > length; i--) {
+                message = message.substring(0, message.length() - 1);
+            }
+        return message + (stretchEnd != null ? stretchEnd : "");
+    }
+
+    public String stretchLeft(String message, int length) {
+        return stretchLeft(message, length, null);
+    }
+
+    public String stretchRight(String message, int length, String stretchEnd) {
+        if (message.length() < length)
+            for (int i = message.length(); i < length; i++) {
+                message = " " + message;
+            }
+        else
+            for (int i = message.length(); i > length; i--) {
+                message = message.substring(1);
+            }
+        return message + (stretchEnd != null ? stretchEnd : "");
+    }
+
+    public String stretchRight(String message, int length) {
+        return stretchRight(message, length, null);
+    }
+
+    public static TerminalConsole getConsole() {
+        return INSTANCE;
     }
 
 }
