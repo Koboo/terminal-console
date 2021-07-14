3e459c8773cebbdc0af9d84ce0004d50711d57a3
Koboo <jakob.koerber@gmail.com>
2021-07-14 08:07:56 +0200

    Delete ICommand interface

--- src/main/java/eu/koboo/terminal/command/Command.java
+++ src/main/java/eu/koboo/terminal/command/Command.java
@@ -2,11 +2,31 @@ package eu.koboo.terminal.command;
 
 import eu.koboo.terminal.TerminalConsole;
 
-public abstract class Command implements ICommand {
+public abstract class Command {
 
     protected final TerminalConsole terminal;
 
     public Command() {
         this.terminal = TerminalConsole.getConsole();
     }
+
+    /**
+     * The array of the commands to enter
+     * to execute the specific TerminalCommand
+     * @return The commands as String[]
+     */
+    public abstract String[] commands();
+
+    /**
+     * The execute method of the TerminalCommand
+     * @param command The entered command
+     * @param args The arguments of the command
+     */
+    public abstract void execute(String command, String[] args);
+
+    /**
+     * Get the description of the specific TerminalCommand
+     * @return The description
+     */
+    public abstract String description();
 }
