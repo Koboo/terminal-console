7133c9996882fcc905e513f57d4983ec63f09144
Koboo <jakob.koerber@gmail.com>
2021-07-14 08:07:44 +0200

    Add shutdownhook to stop command

--- src/main/java/eu/koboo/terminal/defaults/CommandStop.java
+++ src/main/java/eu/koboo/terminal/defaults/CommandStop.java
@@ -12,6 +12,9 @@ public class CommandStop extends Command {
     @Override
     public void execute(String command, String[] args) {
         terminal.all("Stopping..");
+        if (terminal.getShutdownHook() != null) {
+            terminal.getShutdownHook().run();
+        }
         System.exit(0);
     }
 
